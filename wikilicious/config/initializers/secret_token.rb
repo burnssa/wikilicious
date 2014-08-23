7911ac6736bf299637c8e1ee0086b761dbe98069
burnssa@gmail.com <burnssa@gmail.com>
2014-08-23 13:59:51 -0700

    users and wikis created

---
+++ wikilicious/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikilicious::Application.config.secret_key_base = '80d182cbcb55465fd52cc5b20cc14f272adab16afc48dd3e52850d9b6a55e2dabeba799978f6513998c8a6ae1b1c3e07b8def93b44f851241857f07ffbf3399d'
